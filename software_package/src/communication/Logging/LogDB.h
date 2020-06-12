396813269ba8da87169e92543e5e133a44224d22
Finn <sarrgies@gmail.com>
2020-06-12 19:56:00 +1200

    created all cpp and h classes. written basic pseudocode for headers.

---
+++ software_package/src/communication/Logging/LogDB.h
@@ -0,0 +1,27 @@
+#ifndef LOGDB_H
+#define LOGDB_H
+
+class LogDB {
+  public:
+    //constructor / destructor
+    LogDB()
+    ~LogDB()
+
+    //fields
+    //Some Kind kind of concrete storage of events
+
+    //methods
+    int getData(){
+      //TODO
+    }
+
+    int writeToSD(){
+      //TODO
+    }
+
+    int addToDB(/*Data type here*/){
+      //TODO
+    }
+}
+
+#endif //LOGDB_H
---
+++ software_package/src/communication/Radio/RadioInterface.h
@@ -0,0 +1,16 @@
+#ifndef RADIOINTERFACE_H
+#define RADIOINTERFACE_H
+
+class RadioInterface {
+  public:
+    //constructor / destructor
+    Logger()
+    ~Logger()
+
+    //methods
+    virtual int runDiagnostics() = 0;
+    virtual int getPostFlightData() = 0;
+    virtual int getInflightData() = 0;
+}
+
+#endif //RADIOINTERFACE_H
