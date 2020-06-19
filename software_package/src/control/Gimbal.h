de71b3dd149a7db07e0a3d74eb7af1fb12b56b0e
Zac Scott <scottzach1@myvuw.ac.nz>
2020-06-19 12:00:10 +1200

    Formats Gimbal files to show valid C++ code for cpplint

--- software_package/src/control/Gimbal.h
+++ software_package/src/control/Gimbal.h
@@ -1,28 +1,23 @@
-#ifndef GIMBAL_H
-#define GIMBAL_H
+#ifndef SOFTWARE_PACKAGE_SRC_CONTROL_GIMBAL_H_
+#define SOFTWARE_PACKAGE_SRC_CONTROL_GIMBAL_H_
 
 #include "Servo.h"
 
 /**
  * Interfaces with the gimbal on the rocket.
 **/
-class Gimbal
-{
-
-private:
-    
+class Gimbal {
+ private:
     /**
      * The servos on the rocket.
     **/
     Servo servos[];
 
-public:
-
+ public:
     /**
      * Constructor
     **/
-    Gimbal(...);
-
+    Gimbal();
     /**
      * Destructor
     **/
@@ -41,7 +36,6 @@ public:
      * @return TODO (Something that returns the current orientation of the servo)
     **/
     void readServos();
-
 };
 
-#endif //GIMBAL_H
+#endif  // SOFTWARE_PACKAGE_SRC_CONTROL_GIMBAL_H_
