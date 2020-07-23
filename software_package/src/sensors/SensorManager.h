21323e3f216375ddd2baf0774ef47ce27bc45fbc
Ron Crisostomo <crisosron@ecs.vuw.ac.nz>
2020-07-23 16:37:34 +1200

    Made more parts of sensors package compliant with google style guide/cpplint

--- software_package/src/sensors/SensorManager.h
+++ software_package/src/sensors/SensorManager.h
@@ -2,64 +2,63 @@
 // Created by Ron Crisostomo on 12/06/20.
 //
 
-#ifndef SENSORMANAGER_H
-#define SENSORMANAGER_H
+#ifndef SOFTWARE_PACKAGE_SRC_SENSORS_SENSORMANAGER_H_
+#define SOFTWARE_PACKAGE_SRC_SENSORS_SENSORMANAGER_H_
 
-#include "sensors.h"
+#include <string>
+#include <vector>
+
 #include "Battery.h"
-#include "IMU.h"
 #include "GPS.h"
+#include "IMU.h"
+#include "/sensors.h"
 
 using std::string;
 using std::vector;
+
 namespace sensors {
-    class SensorManager {
-    public:
-        static const int NUM_SENSORS = 3; //TODO:
+class SensorManager {
+ public:
+  static const int NUM_SENSORS = 3;  // TODO(crisosron): Determine correct val
 
-        /**
-         * Entry point of SensorManager. This will perform any necessary setup for all sensors
-        */
-        void initSensors();
+  /**
+   * Entry point of SensorManager. This will perform any necessary setup for all
+   * sensors
+   */
+  void initSensors();
 
-        /**
-         * Obtains the current reading on the battery sensor
-         * @return A SensorReading structure instance with a timestamp and reading
-         */
-        SensorReading readBattery() const;
+  /**
+   * Obtains the current reading on the battery sensor
+   * @return A SensorReading structure instance with a timestamp and reading
+   */
+  SensorReading readBattery() const;
 
-        /**
-         * Obtains the current reading on the IMU sensor
-         * @return A SensorReading instance with a timestamp and reading
-         */
-        SensorReading readIMU() const;
+  /**
+   * Obtains the current reading on the IMU sensor
+   * @return A SensorReading instance with a timestamp and reading
+   */
+  SensorReading readIMU() const;
 
-        /**
-         * Obtains the current reading on the GPS sensor
-         * @return A SensorReading instance with a timestamp and reading
-         */
-        SensorReading readGPS() const;
+  /**
+   * Obtains the current reading on the GPS sensor
+   * @return A SensorReading instance with a timestamp and reading
+   */
+  SensorReading readGPS() const;
 
-        inline Battery * getBatterySensor() const {
-            return batterySensor;
-        };
+  inline Battery* getBatterySensor() const { return batterySensor; }
 
-        inline IMU * getIMUSensor() const {
-            return imuSensor;
-        };
+  inline IMU* getIMUSensor() const { return imuSensor; }
 
-        inline GPS * getGPSSensor() const {
-            return gpsSensor;
-        };
+  inline GPS* getGPSSensor() const { return gpsSensor; }
 
-    private:
-        vector<AbstractSensor> sensors;
+ private:
+  vector<AbstractSensor> sensors;
 
-        // Note that these are pointers to AbstractSensor instances - This was done on purpose to control when each sensor is initialized
-        Battery * batterySensor;
-        IMU * imuSensor;
-        GPS * gpsSensor;
-
-    };
-}
-#endif //SENSORMANAGER_H
+  // Note that these are pointers to AbstractSensor instances - This was done on
+  // purpose to control when each sensor is initialized
+  Battery* batterySensor;
+  IMU* imuSensor;
+  GPS* gpsSensor;
+};
+}  // namespace sensors
+#endif  // SOFTWARE_PACKAGE_SRC_SENSORS_SENSORMANAGER_H_
--- software_package/src/sensors/main.cpp
+++ software_package/src/sensors/main.cpp
@@ -1,51 +1,59 @@
-#include <iostream>
-#include <ctime>
 #include <unistd.h>
-#include "assert.h"
+
+#include <ctime>
+#include <iostream>
+#include <cassert>
 #include "/SensorManager.h"
 
-using namespace sensors;
+using sensors::AbstractSensor;
 using std::cout;
 using std::endl;
 
-void printReadings(SensorManager sensorManager, AbstractSensor * sensor, 
-int numReadingsToDisplay) {
-
-    // Displaying some readings samples
-    cout << "---------------------------- Displaying " << numReadingsToDisplay << " readings for " << sensor->getName() << "----------------------------" << endl;
-    for(int i = 0; i < numReadingsToDisplay; i++) {
-        SensorReading sensorReading = sensor -> getSensorReadings()[i];
-        cout << sensor -> getName() << " reading - timestamp: " << std::ctime(&(sensorReading.timestamp)) << " value: " << sensorReading.value << endl;
-    }
+void printReadings(SensorManager sensorManager, AbstractSensor* sensor,
+                   int numReadingsToDisplay) {
+  // Displaying some readings samples
+  cout << "---------------------------- Displaying " << numReadingsToDisplay
+       << " readings for " << sensor->getName()
+       << "----------------------------" << endl;
+  for (int i = 0; i < numReadingsToDisplay; i++) {
+    SensorReading sensorReading = sensor->getSensorReadings()[i];
+    cout << sensor->getName()
+         << " reading - timestamp: " << std::ctime(&(sensorReading.timestamp))
+         << " value: " << sensorReading.value << endl;
+  }
 }
 
 int main() {
-    SensorManager sensorManager;
-    sensorManager.initSensors();
+  SensorManager sensorManager;
+  sensorManager.initSensors();
 
-    int numReads = 50;
+  int numReads = 50;
 
-    // Performing numReads reads on each sensor
-    cout << "Performing " << numReads << " on each sensor" << endl;
-    for(int i = 0; i < numReads; i++){
-        sensorManager.readBattery();
-        sensorManager.readIMU();
-        sensorManager.readGPS();
-        usleep(250000); // Sleep for 1/4 second
-    }
+  // Performing numReads reads on each sensor
+  cout << "Performing " << numReads << " on each sensor" << endl;
+  for (int i = 0; i < numReads; i++) {
+    sensorManager.readBattery();
+    sensorManager.readIMU();
+    sensorManager.readGPS();
+    usleep(250000);  // Sleep for 1/4 second
+  }
 
-    // Checking that reads were performed
-    cout << "Asserting all read values are stored by each sensor " << endl;
-    assert(sensorManager.getBatterySensor() -> getSensorReadings().size() == numReads);
-    assert(sensorManager.getIMUSensor() -> getSensorReadings().size() == numReads);
-    assert(sensorManager.getGPSSensor() -> getSensorReadings().size() == numReads);
-    cout << "Assertions passed " << endl;
+  // Checking that reads were performed
+  cout << "Asserting all read values are stored by each sensor " << endl;
+  assert(sensorManager.getBatterySensor()->getSensorReadings().size() ==
+         numReads);
+  assert(sensorManager.getIMUSensor()->getSensorReadings().size() == numReads);
+  assert(sensorManager.getGPSSensor()->getSensorReadings().size() == numReads);
+  cout << "Assertions passed " << endl;
 
-    // Displaying some readings samples
-    int numReadingsToDisplay = 10;
-    printReadings(sensorManager, sensorManager.getBatterySensor(), numReadingsToDisplay);
-    printReadings(sensorManager, sensorManager.getIMUSensor(), numReadingsToDisplay);
-    printReadings(sensorManager, sensorManager.getGPSSensor(), numReadingsToDisplay);
-    
-    return 0;
+  // Displaying some readings samples
+  int numReadingsToDisplay = 10;
+  printReadings(sensorManager, sensorManager.getBatterySensor(),
+                numReadingsToDisplay);
+  printReadings(sensorManager, sensorManager.getIMUSensor(),
+                numReadingsToDisplay);
+  printReadings(sensorManager, sensorManager.getGPSSensor(),
+                numReadingsToDisplay);
+
+  return 0;
 }
