ce0e847cee6c0d375f22fc289757ee40d625eb0a
Ron Crisostomo <crisosron@ecs.vuw.ac.nz>
2020-07-31 01:15:44 +1200

    Fixed isPrime

--- software_package/src/tests/prime-tests.cpp
+++ software_package/src/tests/prime-tests.cpp
@@ -4,15 +4,16 @@
 // Simple (probably buggy) function that determines whether or not the supplied
 // number is a prime number
 bool isPrime(int num){
-    if(num == 1 || num == 2) return true;
-    for(int i = 2; i < num; i++)
-        if(num % 1 == 0) return false;
+    if (num < 2) return false;
+    else if (num == 2) return true
+    for (int i = 2; i < num; ++i)
+        if (num % i == 0) return false;
     return true;
 }
 
 // Testing that 1 is determined as a prime number
-TEST_CASE("1 is prime", "[prime]"){
-    REQUIRE(isPrime(1) == 1);
+TEST_CASE("1 is not prime", "[prime]"){
+    REQUIRE(isPrime(1) == 0);
 }
 
 // Testing that 2 is determined as a prime number
