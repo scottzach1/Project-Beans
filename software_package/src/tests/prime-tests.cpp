c004e080309b7fad95809c4e5286868acdaeca0e
Ron Crisostomo <crisosron@ecs.vuw.ac.nz>
2020-07-31 12:28:47 +1200

    Fixed typo. Added gcov/gcovr metadata to gitignore

--- software_package/src/tests/prime-tests.cpp
+++ software_package/src/tests/prime-tests.cpp
@@ -5,7 +5,7 @@
 // number is a prime number
 bool isPrime(int num){
     if (num < 2) return false;
-    else if (num == 2) return true
+    else if (num == 2) return true;
     for (int i = 2; i < num; ++i)
         if (num % i == 0) return false;
     return true;
