a2ca5b5bf875d5c6b02d206cf24f3bd8ec516dd8
Zac Scott <scottzach1@myvuw.ac.nz>
2020-09-14 15:28:33 +1200

    Implements template for PID controller.
    In this commit, we create a compiling template
    for the PID controller functionality of the rocket.
    We introduce an inner class containing the basis of
    PID functionality. For each of these, they act as a
    wrapper around a Servo component. Each of these will
    be called to step and handle both the X and Y axis
    servos individually upon GuidanceSystem::step_pid().
    
    HELP: I seem to be getting quite a few issues
    regarding initializing the PID objects (relating
    to deleted function). I will need to do more reading
    into this, I assume this will automatically be
    initialized if I cannot manually perform this action?

--- software_package/development/lib/software/src/guidance_system.cpp
+++ software_package/development/lib/software/src/guidance_system.cpp
@@ -1,11 +1,65 @@
-
 #include "guidance_system.h"
+#include "limits"
+
 
 GuidanceSystem::GuidanceSystem() {
+    servo_x = Servo();  // pass required args.
+    servo_y = Servo();  // pass required args.
+
+    pid_x.add_servo(&servo_x);
+    pid_y.add_servo(&servo_y);
+}
+
+GuidanceSystem::~GuidanceSystem() {
+    delete &servo_x;
+    delete &servo_y;
+
+    delete &pid_x;
+    delete &pid_y;
 }
 
 void GuidanceSystem::step_pid() {
+    pid_x.step_pid();
 }
 
 void GuidanceSystem::launch_parachute() {
+    Serial.printf("TODO: launch parachute!");
+}
+
+
+void GuidanceSystem::PID::add_servo(Servo *s) {
+    servo = s;
+}
+
+void GuidanceSystem::PID::step_pid() {
+    // TODO(scottzach1): Switch with new Servo method once established.
+    // float angle = servo->read_val();
+    float angle = std::numeric_limits<float>::signaling_NaN();
+    // Time
+    uint32_t current_time = millis();
+    uint32_t elapsed_time = current_time - timestamp;
+    // Calculate PID
+    // P
+    float current_error = TARGET_ANGLE - angle;
+    // I
+    cumulative_error += current_error * elapsed_time;
+    // D
+    float derivative_error = (current_error - previous_error) / elapsed_time;
+
+    // Generate output.
+    float result =
+        (P * current_error) + (I * cumulative_error) + (D * derivative_error);
+    previous_error = current_error;
+
+    // Transform to safe motor output.
+    result = santize_value(result);
+
+    // Update the servo (function missing).
+    // if (servo) servo->set_val(result);
+}
+
+int8_t GuidanceSystem::PID::santize_value(float input) {
+    // TODO(scottzach1): Perform sanitisation of PID output, that is readable
+    // by the rocket. Should output within range '-128 < output 128'.
+    return (int8_t) input;
 }
--- software_package/development/lib/software/src/guidance_system.h
+++ software_package/development/lib/software/src/guidance_system.h
@@ -1,31 +1,63 @@
 #ifndef SOFTWARE_PACKAGE_DEVELOPMENT_LIB_SOFTWARE_SRC_GUIDANCE_SYSTEM_H_
 #define SOFTWARE_PACKAGE_DEVELOPMENT_LIB_SOFTWARE_SRC_GUIDANCE_SYSTEM_H_
 
-
+#include "Arduino.h"
+#include "servo.h"
 
 /**
  *Library which handles interactions with the Lora.
-**/
+ **/
 class GuidanceSystem {
+    class PID {
+     private:
+        uint32_t timestamp = millis();
+        float previous_error = 0;
+        float cumulative_error = 0;
+        Servo *servo;
+
+     public:
+        const float TARGET_ANGLE = -90;
+        const float P = 0.5;
+        const float I = 0.5;
+        const float D = 0.5;
+
+        PID() = default;
+        ~PID() = default;
+        void add_servo(Servo *s);
+        void step_pid();
+        int8_t santize_value(float input);
+        boolean error();
+    };
+
+ private:
+    Servo servo_x;
+    Servo servo_y;
+
+    // FIXME(scottzach1): Is this automatically initaiized? I get
+    // deleted functon error when I manually initialize using its
+    // own constructor.
+    PID pid_x;
+    PID pid_y;
+
  public:
     /**
      * Constructor
-    **/
+     **/
     GuidanceSystem();
     /**
      * Destructor
-    **/
+     **/
     ~GuidanceSystem();
 
     /**
      * Step through the pid controller once.
-    **/
+     **/
     void step_pid();
 
     /**
      * Call to launch the parachute.
      * TODO(sargisfinl) - State changes.
-    **/
+     **/
     void launch_parachute();
 };
 
