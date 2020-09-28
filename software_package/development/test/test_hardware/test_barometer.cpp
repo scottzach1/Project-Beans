61f22bb85e7b6399855b727df5bfeab426b8a2ee
Finn <sarrgies@gmail.com>
2020-09-28 14:37:26 +1300

    completed barometer pre-launch tests

--- software_package/development/test/test_hardware/test_barometer.cpp
+++ software_package/development/test/test_hardware/test_barometer.cpp
@@ -1,7 +1,20 @@
 #include "unity.h"
+// #include "barometer.h"
+
+
+ 
+
+// values found according to https://www.freemaptools.com/elevation-finder.htm
+// currently based on Trentham Memorial Park launch site, this value will need to be changed if the launch site changes.
+float realistic_height = 50.0;
+// buffer for range is height is only an estimate, and barometer has been known to be inconsistent
+float height_buffer = 40.0;
+
 
 void setup(void) {
   // set up to perform before every test
+  Barometer baro;
+  baro::init();
 }
 
 void teardown(void) {
@@ -9,11 +22,20 @@ void teardown(void) {
 }
 
 void test_read_altitude() {
-  // Make sure that the altitude can be read
+  // Make sure that the altitude can be read, and is within the expected height boundary.
+  float alt = baro::read_altitude();
+  TEST_ASSERT_GREATER_OR_EQUAL(alt, (realistic_height - height_buffer));
+  TEST_ASSERT_LESS_OR_EQUAL(alt, (realistic_height + height_buffer));
 }
 
 void test_read_pressure() {
   // Make sure that the pressure can be read
+  float pressure_bars = baro::read_pressure_bars();
+  float pressure_pascal = baro::read_pressure_pascal();
+  float pressure_psi = baro::read_pressure_psi();
+  UNITY_TEST_ASSERT_FLOAT_IS_NOT_NAN(pressure_bars);
+  UNITY_TEST_ASSERT_FLOAT_IS_NOT_NAN(pressure_pascal);
+  UNITY_TEST_ASSERT_FLOAT_IS_NOT_NAN(pressure_psi);
 }
 
 int main() {
