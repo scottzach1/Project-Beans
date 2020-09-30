9e0ef7205dcda65b7990113af6a51ae3e92c8e3a
Zac Scott <scottzach1@myvuw.ac.nz>
2020-09-30 18:49:16 +1300

    Sets the Servo to a given angle via code.
    In this commit, we add mildly tweaked sample code from the
    library that can set both Servo's to a given angle.
    
    For some stupid reason, I cannot seem get the `Servo` library
    to work for me when installed as a project library. We got
    around this for now by copying it across to the global library
    directory: `~/.platformio/lib/...`.

--- software_package/testing/servo_testing/src/main.cpp
+++ software_package/testing/servo_testing/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo servoGreen;  // create servo object to control a servo
+Servo servoPink;
+
+int potpin = 0;
+int val;
 
 void setup() {
-  // put your setup code here, to run once:
+    servoGreen.attach(A3, 1000, 2500);
+    servoPink.attach(A2, 1000, 2500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Library Example Code
+    // val = analogRead(potpin);
+    // val = 10;
+    // val = map(val, 0, 1023, 0, 180);  // 30
+    // myservo.write(val);
+
+    // Working
+    servoGreen.write(90);
+    servoPink.write(90);
+    delay(15);
 }
