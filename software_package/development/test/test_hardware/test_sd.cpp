35c864c11d876723e0afb7770c9785515e946352
Zac Scott <scottzach1@myvuw.ac.nz>
2020-10-16 23:34:24 +1300

    Fixes conflicts regarding sd_card testing.

--- software_package/development/test/test_hardware/test_sd.cpp
+++ software_package/development/test/test_hardware/test_sd.cpp
@@ -61,35 +61,34 @@ void test_sd_write() {
 }
 
 void test_sd_file_write() {
-    myFile = SD.open("test.txt", FILE_WRITE);
-    std::string test_string = "Example code being written";
+  myFile = SD.open("test.txt", FILE_WRITE);
+  std::string test_string = "Example code being written";
 
-    // write to file
-    if (myFile) {
-        myFile.println(test_string);
-        myFile.close();
-    } else {
-        // error state
-        TEST_FAIL_MESSAGE("error opening test.txt (w)");
-    }
+  // write to file
+  if (myFile) {
+    myFile.println(test_string);
+    myFile.close();
+  } else {
+    // error state
+    TEST_FAIL_MESSAGE("error opening test.txt (w)");
+  }
 
-    // re-open the file for reading and verification
-    myFile = SD.open("test.txt");
-    if (myFile) {
-        std::string out = "";
-        while (myFile.available()) {
-            out.append(myFile.read());
-        }
-        // close the file:
-        myFile.close();
-        TEST_ASSERT_EQUAL(out, test_string);
-    } else {
-        // ierror state
-        TEST_FAIL_MESSAGE("error opening test.txt (r)");
+  // re-open the file for reading and verification
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    std::string out = "";
+    while (myFile.available()) {
+      out.append(myFile.read());
     }
-    Serial.println("###### End of the SD tests ######");
+    // close the file:
+    myFile.close();
+    TEST_ASSERT_EQUAL(out, test_string);
+  } else {
+    // ierror state
+    TEST_FAIL_MESSAGE("error opening test.txt (r)");
+  }
+  Serial.println("###### End of the SD tests ######");
 }
-
 
 void process() {
   UNITY_BEGIN();  // these sleeps may need tweaking!
@@ -99,19 +98,6 @@ void process() {
 }
 
 #ifdef ARDUINO
-
-// void setup() {
-//     // NOTE!!! Wait for >2 secs
-//     // if board doesn't support software reset via Serial.DTR/RTS
-//     delay(5000);
-
-//     Serial.begin(115200);
-
-//     while (!Serial)
-//         delay(10);  // will pause Zero, Leonardo, etc until serial console opens
-
-//     process();
-// }
 
 void setup() {
   // NOTE!!! Wait for >2 secs
@@ -132,13 +118,13 @@ void loop() { delay(1); }
 
 #else
 
-int main(){
+int main() {
   // These tests are more of a 'pre-test' that verify that we are
   // correctly interfacing with the adafruit libraries for the
   // hardware package
   UNITY_BEGIN();
-//   RUN_TEST(test_sd);
-//   RUN_TEST(test_sd_write);
+  //   RUN_TEST(test_sd);
+  //   RUN_TEST(test_sd_write);
   RUN_TEST(test_sd_file_write);
   return UNITY_END();
 }
